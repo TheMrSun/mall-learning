93917d972b9b81c62896f303b81634d0a0e1d636
Zhaocy <2890554485@qq.com>
2022-04-10 12:02:06 +0800

    mall-tiny-mybatis init commit

---
+++ mall-tiny-mybatis/src/main/java/com/marco/mall/malltinymybatis/MallTinyMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.marco.mall.malltinymybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Slience
+ */
+@SpringBootApplication
+public class MallTinyMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyMybatisApplication.class, args);
+    }
+
+}
---
+++ mall-tiny-mybatis/src/test/java/com/marco/mall/malltinymybatis/MallTinyMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.mall.malltinymybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTinyMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
